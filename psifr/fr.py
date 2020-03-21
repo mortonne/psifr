240831e3eb67733a2ecaa5d57ca89d4256fe91bf
Neal Morton <mortonne@gmail.com>
2020-03-21 16:38:26 -0500

    basic alignment and scoring functionality for free recall data

---
+++ psifr/fr.py
@@ -0,0 +1,123 @@
+"""Utilities for working with free recall data."""
+
+import pandas as pd
+
+
+def check_data(df):
+    """Run checks on free recall data.
+
+    Parameters
+    ----------
+    df : pandas.DataFrame
+        Contains one row for each trial (study and recall). Must have fields:
+            subject : number or str
+                Subject identifier.
+            list : number
+                List identifier. This applies to both study and recall trials.
+            trial_type : str
+                Type of trial; may be 'study' or 'recall'.
+            position : number
+                Position within the study list or recall sequence.
+            item : str
+                Item that was either presented or recalled on this trial.
+    """
+
+    # check that all fields are accounted for
+    columns = ['subject', 'list', 'trial_type', 'position', 'item']
+    for col in columns:
+        assert col in df.columns, f'Required column {col} is missing.'
+
+    # only one column has a hard constraint on its exact content
+    assert df['trial_type'].isin(['study', 'recall']).all(), (
+        'trial_type for all trials must be "study" or "recall".')
+
+
+def merge_lists(df, merge_keys=None, list_keys=None, position_key='position'):
+    """Merge study and recall events together for each list.
+
+    Parameters
+    ----------
+    df : pandas.DataFrame
+        Information about all study and recall events, potentially for
+        multiple lists and/or subjects. Each row corresponds to one
+        event. Must include a 'trial_type' column with values 'study'
+        for study events and 'recall' for recall events (that is,
+        attempted recalls).
+
+    merge_keys : list, optional
+        Columns to use to designate events to merge. Default is
+        ['subject', 'list', 'item'], which will merge events related to
+        the same item, but only within list.
+
+    list_keys : list, optional
+        Columns that apply to an entire list, as opposed to specific
+        study events. These columns will still be defined for
+        intrusions even though they have no corresponding study event.
+
+    position_key : str, optional
+        Column indicating the position of each item in either the study
+        list or the recall sequence.
+
+    Returns
+    -------
+    merged : pandas.DataFrame
+        Merged information about study and recall events. Each row
+        corresponds to one unique input/output pair.
+
+        The following columns will be added:
+        input : int
+            Position of each item in the input list (i.e., serial
+            position).
+        output : int
+            Position of each item in the recall sequence.
+        recalled : bool
+            True for rows with an associated recall event.
+        repeat : int
+            Number of times this recall event has been repeated (0 for
+            the first recall of an item).
+        intrusion : bool
+            True for recalls that do not correspond to any study event.
+    """
+
+    if merge_keys is None:
+        merge_keys = ['subject', 'list', 'item']
+
+    # keys that define a unique recall but not its output position
+    rec_keys = merge_keys + ['trial_type']
+
+    # get running count of number of times each item is recalled in each list
+    # also applies to study events, but leaving those out now for simplicity
+    # as that will only apply to studies with repeated presentations
+    df.loc[:, 'repeat'] = df.groupby(rec_keys).cumcount()
+
+    # get study and recall events
+    study = df.loc[df['trial_type'] == 'study']
+    recall = df.loc[df['trial_type'] == 'recall']
+
+    # set keys to define the level of recall at items within list
+    if list_keys is not None:
+        merge_keys += list_keys
+
+    # get just the fields to use in the merge
+    study = study.drop(columns=['trial_type', 'repeat'])
+    recall = recall[merge_keys + ['position', 'repeat']]
+
+    # merge information from study and recall trials
+    merged = pd.merge(study, recall, left_on=merge_keys, right_on=merge_keys,
+                      how='outer')
+
+    # position from study events indicates input position;
+    # position from recall events indicates output position
+    merged = merged.rename(columns={position_key + '_x': 'input',
+                                    position_key + '_y': 'output'})
+
+    # field to indicate whether a given item was recalled
+    merged.loc[:, 'recalled'] = merged['output'].notna().astype('bool')
+
+    # field to indicate whether a given recall was an intrusion
+    merged.loc[:, 'intrusion'] = merged['input'].isna().astype('bool')
+
+    # fix repeats field to define for non-recalled items
+    merged.loc[merged['repeat'].isna(), 'repeat'] = 0
+    merged = merged.astype({'repeat': 'int'})
+    return merged
