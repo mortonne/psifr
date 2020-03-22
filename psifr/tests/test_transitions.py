6e3e98bcee273034aabe7088b50e2c6e4e5b052c
Neal Morton <mortonne@gmail.com>
2020-03-22 02:12:06 -0500

    streamline by initializing actual and possible series objects

--- psifr/tests/test_transitions.py
+++ psifr/tests/test_transitions.py
@@ -8,26 +8,26 @@ class TransitionsTestCase(unittest.TestCase):
 
     def setUp(self) -> None:
         self.seq = [1, 3, 4, 8, 5, 4, 7, 6]
-        self.possible = list(range(1, 9))
+        self.all_possible = list(range(1, 9))
         self.lags = list(range(-7, 8))
         self.from_mask = np.array([1, 1, 1, 0, 1, 1, 1, 1], dtype=bool)
         self.to_mask = np.array([1, 1, 0, 1, 1, 1, 1, 1], dtype=bool)
+        self.actual = pd.Series(0, dtype='int', index=self.lags)
+        self.possible = pd.Series(0, dtype='int', index=self.lags)
 
     def test_lag(self):
-        masker = fr.transition_masker(self.seq, self.possible,
+        masker = fr.transition_masker(self.seq, self.all_possible,
                                       self.from_mask, self.to_mask)
-        actual = pd.Series(0, dtype='int', index=self.lags)
-        possible = pd.Series(0, dtype='int', index=self.lags)
         for prev, curr, poss in masker:
-            actual[curr - prev] += 1
-            possible[np.subtract(poss, prev)] += 1
+            self.actual[curr - prev] += 1
+            self.possible[np.subtract(poss, prev)] += 1
 
         np.testing.assert_array_equal(
-            actual.to_numpy(),
+            self.actual.to_numpy(),
             np.array([0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 0]))
 
         np.testing.assert_array_equal(
-            possible.to_numpy(),
+            self.possible.to_numpy(),
             np.array([0, 0, 1, 0, 0, 1, 1, 0, 2, 2, 2, 2, 1, 1, 1]))
 
 
