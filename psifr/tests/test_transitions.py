1a45692f6df582acae7cb894ca5717263450b0bb
Neal Morton <mortonne@gmail.com>
2020-03-22 02:12:26 -0500

    test lag results when using no mask

--- psifr/tests/test_transitions.py
+++ psifr/tests/test_transitions.py
@@ -30,6 +30,20 @@ class TransitionsTestCase(unittest.TestCase):
             self.possible.to_numpy(),
             np.array([0, 0, 1, 0, 0, 1, 1, 0, 2, 2, 2, 2, 1, 1, 1]))
 
+    def test_lag_no_mask(self):
+        masker = fr.transition_masker(self.seq, self.all_possible)
+        for prev, curr, poss in masker:
+            self.actual[curr - prev] += 1
+            self.possible[np.subtract(poss, prev)] += 1
+
+        np.testing.assert_array_equal(
+            self.actual.to_numpy(),
+            np.array([0, 0, 0, 0, 1, 0, 1, 0, 1, 1, 0, 1, 0, 0, 0]))
+
+        np.testing.assert_array_equal(
+            self.possible.to_numpy(),
+            np.array([0, 1, 1, 0, 1, 2, 3, 0, 3, 3, 3, 3, 2, 1, 1]))
+
 
 if __name__ == '__main__':
     unittest.main()
