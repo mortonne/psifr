7e253ca19cea3acdf0b4d86d04600cf8a7dedf67
Neal Morton <mortonne@gmail.com>
2020-03-28 18:15:20 -0500

    draft of new transitions masker

--- psifr/transitions.py
+++ psifr/transitions.py
@@ -4,30 +4,29 @@ import numpy as np
 import pandas as pd
 
 
-def transitions_masker(outputs, n_recall, from_mask, to_mask,
-                       test_values=None, test=None):
+def transitions_masker(pool, output, pool_test=None, output_test=None,
+                       test=None):
     """Iterate over transitions with masking.
 
     Parameters
     ----------
-    outputs : numpy.array
-        Values to output for each transition.
+    pool : list
+        Items available for recall. Order does not matter. May contain
+        repeated values.
 
-    n_recall : int
-        Number of recall attempts.
+    output : list
+        Recalled items in output position order.
 
-    from_mask : numpy.array
-        Boolean array indicating valid positions to transition from.
+    pool_test : list
+        Test values for items available for recall. Must be the same
+        order as pool.
 
-    to_mask : numpy.array
-        Boolean array indicating valid positions to transition to.
-
-    test_values : numpy.array
-        Same shape and order as `outputs`. Used to test whether
-        individual transitions should be included.
+    output_test : list
+        Test values for items in output position order.
 
     test : callable
-        Used to test whether individual transitions should be included.
+        Used to test whether individual transitions should be included,
+        based on test values.
             test(prev, curr) - test for included transition
             test(prev, poss) - test for included possible transition
 
@@ -43,45 +42,40 @@ def transitions_masker(outputs, n_recall, from_mask, to_mask,
         Output values for all possible valid "to" items.
     """
 
-    # list of all valid outputs
-    valid_outputs = outputs[to_mask]
-    if test_values is not None:
-        valid_values = test_values[to_mask]
-
-    # counter for recall and counter for valid recall
     n = 0
-    m = 0
-    while n < n_recall - 1:
-        # check if the positions involved in this transition are valid
-        if not from_mask[n] or not to_mask[n + 1]:
+    possible = pool.copy()
+    if test is not None:
+        possible_test = pool_test.copy()
+
+    while n < len(output) - 1:
+        # test if the previous item is in the pool
+        if output[n] not in possible:
             n += 1
-            if to_mask[n]:
-                m += 1
             continue
 
-        # valid next items at this output position
-        step_outputs = valid_outputs[m + 1:]
+        # remove the item from the pool
+        ind = possible.index(output[n])
+        del possible[ind]
+        if test is not None:
+            del possible_test[ind]
 
-        if test_values is not None:
-            # check if this transition is included
-            if not test(test_values[n], test_values[n + 1]):
+        # test if the current item is in the pool
+        if output[n + 1] not in possible:
+            n += 1
+            continue
+
+        prev = output[n]
+        curr = output[n + 1]
+        poss = np.array(possible)
+        if test is not None:
+            # test if this transition is included
+            if not test(output_test[n], output_test[n + 1]):
                 n += 1
-                if to_mask[n]:
-                    m += 1
                 continue
 
-            # get valid possible recalls that are included
-            step_values = valid_values[m + 1:]
-            poss = step_outputs[test(test_values[n], step_values)]
-        else:
-            poss = step_outputs
-
-        prev = outputs[n]
-        curr = outputs[n + 1]
-
+            # get included possible items
+            poss = poss[test(output_test[n], np.array(possible_test))]
         n += 1
-        if to_mask[n]:
-            m += 1
         yield prev, curr, poss
 
 
