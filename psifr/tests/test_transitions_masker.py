72192b2a733df66afe44751d54ad926522784ba6
Neal Morton <mortonne@gmail.com>
2020-03-28 18:29:00 -0500

    work on testing previous version of transitions masker

---
+++ psifr/tests/test_transitions_masker.py
@@ -0,0 +1,38 @@
+import unittest
+import numpy as np
+import pandas as pd
+from .. import transitions
+
+
+class TransitionsMaskerTestCase(unittest.TestCase):
+
+    def setUp(self) -> None:
+        self.pool_position = [1, 2, 3, 4, 5, 6, 7, 8]
+        self.pool_category = [1, 1, 1, 1, 2, 2, 2, 2]
+        self.output_position = [1, 3, 4, 8, 5, 4, 7, 6]
+        self.output_category = [1, 1, 1, 2, 2, 1, 2, 2]
+
+    def test_position(self):
+        masker = transitions.transitions_masker(self.pool_position,
+                                                self.output_position)
+        trans = [[x, y, z.tolist()] for x, y, z in masker]
+        expected = [[1, 3, [2, 3, 4, 5, 6, 7, 8]],
+                    [3, 4, [2, 4, 5, 6, 7, 8]],
+                    [4, 8, [2, 5, 6, 7, 8]],
+                    [8, 5, [2, 5, 6, 7]],
+                    [7, 6, [2, 6]]]
+        assert trans == expected
+
+    def test_category(self):
+        masker = transitions.transitions_masker(self.pool_category,
+                                                self.output_category)
+        trans = [[x, y, z.tolist()] for x, y, z in masker]
+        expected = [[1, 1, [1, 1, 1, 2, 2, 2, 2]],
+                    [1, 1, [1, 1, 2, 2, 2, 2]],
+                    [1, 2, [1, 2, 2, 2, 2]],
+                    [2, 2, [1, 2, 2, 2]],
+                    [2, 1, [1, 2, 2]],
+                    [1, 2, [2, 2]],
+                    [2, 2, [2]]]
+        import pdb
+        pdb.set_trace()
