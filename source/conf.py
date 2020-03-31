828507622540fcbd0f2f12d9b1a003cf2ff5323b
Neal Morton <mortonne@gmail.com>
2020-03-31 03:53:26 -0500

    change version in documentation to 0.1.0

--- source/conf.py
+++ source/conf.py
@@ -22,7 +22,7 @@ copyright = '2020, Neal Morton'
 author = 'Neal Morton'
 
 # The full version, including alpha/beta/rc tags
-release = 'v1.0.0'
+release = 'v0.1.0'
 
 
 # -- General configuration ---------------------------------------------------
