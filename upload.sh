ebfaf4398360c339ddf43ebe809690553d55c618
Neal Morton <mortonne@gmail.com>
2020-04-05 07:42:21 -0500

    build(pypi): add script to upload to PyPI

---
+++ upload.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+python3 -m pip install --user --upgrade setuptools wheel twine
+python3 setup.py sdist bdist_wheel
+python3 -m twine upload --repository-url https://upload.pypi.org/legacy/ dist/*
