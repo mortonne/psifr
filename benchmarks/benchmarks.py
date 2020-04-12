6f0398cfba1de1163ecd00ceaeffaf7d073b26da
Neal Morton <mortonne@gmail.com>
2020-04-12 08:58:06 -0500

    perf: add benchmarks for spc, category crp

--- benchmarks/benchmarks.py
+++ benchmarks/benchmarks.py
@@ -26,5 +26,11 @@ class TimeFR:
                               list_keys=['list_type', 'list_category'],
                               study_keys=['category'])
 
-    def time_crp(self):
+    def time_lag_crp(self):
         crp = fr.lag_crp(self.data)
+
+    def time_category_crp(self):
+        crp = fr.category_crp(self.data, 'category')
+
+    def time_spc(self):
+        spc = fr.spc(self.data)
