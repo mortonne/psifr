42e5705a4ec6368ee799ca98260798aecdeb75f2
Neal Morton <mortonne@gmail.com>
2020-06-15 12:42:35 -0500

    feat: add distance rank analysis

--- src/psifr/fr.py
+++ src/psifr/fr.py
@@ -470,6 +470,53 @@ def distance_crp(df, index_key, distances, edges, centers=None,
     return crp
 
 
+def distance_rank(df, index_key, distances, item_query=None, test_key=None,
+                  test=None):
+    """
+    Calculate rank of transition distances in free recall lists.
+
+    Parameters
+    ----------
+    df : pandas.DataFrame
+        Merged study and recall data. See merge_lists. List length is
+        assumed to be the same for all lists within each subject.
+        Must have fields: subject, list, input, output, recalled.
+        Input position must be defined such that the first serial
+        position is 1, not 0.
+
+    index_key : str
+        Name of column containing the index of each item in the
+        `distances` matrix.
+
+    distances : numpy.array
+        Items x items matrix of pairwise distances or similarities.
+
+    item_query : str, optional
+        Query string to select items to include in the pool of possible
+        recalls to be examined. See `pandas.DataFrame.query` for
+        allowed format.
+
+    test_key : str, optional
+        Name of column with labels to use when testing transitions for
+        inclusion.
+
+    test : callable, optional
+        Callable that takes in previous and current item values and
+        returns True for transitions that should be included.
+
+    Returns
+    -------
+    stat : pandas.DataFrame
+        Has fields 'subject' and 'rank'.
+    """
+    measure = transitions.TransitionDistanceRank(
+        index_key, distances, item_query=item_query,
+        test_key=test_key, test=test
+    )
+    rank = measure.analyze(df)
+    return rank
+
+
 def category_crp(df, category_key, item_query=None, test_key=None, test=None):
     """
     Conditional response probability of within-category transitions.
