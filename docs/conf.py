0d10172b5d6611191dfc2ef612550058d2e8c0e4
Neal Morton <mortonne@gmail.com>
2020-09-21 04:59:50 -0500

    build: increment patch version

--- docs/conf.py
+++ docs/conf.py
@@ -21,7 +21,7 @@ copyright = '2020, Neal Morton'
 author = 'Neal Morton'
 
 # The full version, including alpha/beta/rc tags
-release = 'v0.4.2'
+release = 'v0.4.3'
 
 
 # -- General configuration ---------------------------------------------------
