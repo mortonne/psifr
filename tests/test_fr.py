b1b302a442dc08fb9e59cc4f6cc78de619fcc521
Neal Morton <mortonne@gmail.com>
2021-03-29 19:05:42 -0500

    style: reformat raw data fixture

--- tests/test_fr.py
+++ tests/test_fr.py
@@ -9,24 +9,37 @@ from psifr import fr
 @pytest.fixture()
 def raw():
     raw = pd.DataFrame(
-        {'subject': [1, 1, 1, 1, 1, 1,
-                     1, 1, 1, 1, 1, 1],
-         'list': [1, 1, 1, 1, 1, 1,
-                  2, 2, 2, 2, 2, 2],
-         'trial_type': ['study', 'study', 'study',
-                        'recall', 'recall', 'recall',
-                        'study', 'study', 'study',
-                        'recall', 'recall', 'recall'],
-         'position': [1, 2, 3, 1, 2, 3,
-                      1, 2, 3, 1, 2, 3],
-         'item': ['absence', 'hollow', 'pupil',
-                  'hollow', 'pupil', 'empty',
-                  'fountain', 'piano', 'pillow',
-                  'pillow', 'fountain', 'pillow'],
-         'item_index': [0, 1, 2, 1, 2, np.nan,
-                        3, 4, 5, 5, 3, 5],
-         'task': [1, 2, 1, 2, 1, np.nan,
-                  1, 2, 1, 1, 1, 1]})
+        {
+            'subject': [
+                1, 1, 1, 1, 1, 1,
+                1, 1, 1, 1, 1, 1
+            ],
+            'list': [
+                1, 1, 1, 1, 1, 1,
+                2, 2, 2, 2, 2, 2
+            ],
+            'trial_type': [
+                'study', 'study', 'study', 'recall', 'recall', 'recall',
+                'study', 'study', 'study', 'recall', 'recall', 'recall',
+            ],
+            'position': [
+                1, 2, 3, 1, 2, 3,
+                1, 2, 3, 1, 2, 3
+            ],
+            'item': [
+                'absence', 'hollow', 'pupil', 'hollow', 'pupil', 'empty',
+                'fountain', 'piano', 'pillow', 'pillow', 'fountain', 'pillow',
+            ],
+            'item_index': [
+                0, 1, 2, 1, 2, np.nan,
+                3, 4, 5, 5, 3, 5,
+            ],
+            'task': [
+                1, 2, 1, 2, 1, np.nan,
+                1, 2, 1, 1, 1, 1,
+            ]
+        }
+    )
     return raw
 
 
