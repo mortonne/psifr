ce3823a165bac1641ec16678ec0bdd88e3c4f46f
Neal Morton <mortonne@gmail.com>
2021-04-16 16:44:10 -0500

    test: refactor to use pytest

--- tests/test_lag_crp.py
+++ tests/test_lag_crp.py
@@ -1,41 +1,47 @@
-import unittest
+import pytest
 import numpy as np
 from psifr import transitions
 
 
-class LagCRPTestCase(unittest.TestCase):
+@pytest.fixture()
+def data():
+    test_list = {
+        'list_length': 8,
+        'pool_position': [1, 2, 3, 4, 5, 6, 7, 8],
+        'pool_category': [1, 1, 1, 1, 2, 2, 2, 2],
+        'output_position': [1, 3, 4, 8, 5, 4, 7, 6],
+        'output_category': [1, 1, 1, 2, 2, 1, 2, 2],
+    }
+    return test_list
 
-    def setUp(self) -> None:
-        self.list_length = 8
-        self.pool_position = [1, 2, 3, 4, 5, 6, 7, 8]
-        self.pool_category = [1, 1, 1, 1, 2, 2, 2, 2]
-        self.output_position = [1, 3, 4, 8, 5, 4, 7, 6]
-        self.output_category = [1, 1, 1, 2, 2, 1, 2, 2]
 
-    def test_lag_count(self):
-        actual, possible = transitions.count_lags(
-            self.list_length, [self.pool_position], [self.output_position],
-        )
+def test_lag_count(data):
+    actual, possible = transitions.count_lags(
+        data['list_length'], [data['pool_position']], [data['output_position']]
+    )
 
-        np.testing.assert_array_equal(
-            actual.to_numpy(),
-            np.array([0, 0, 0, 0, 1, 0, 1, 0, 1, 1, 0, 1, 0, 0, 0]))
+    np.testing.assert_array_equal(
+        actual.to_numpy(), np.array([0, 0, 0, 0, 1, 0, 1, 0, 1, 1, 0, 1, 0, 0, 0])
+    )
 
-        np.testing.assert_array_equal(
-            possible.to_numpy(),
-            np.array([0, 1, 1, 0, 1, 2, 3, 0, 3, 3, 3, 3, 2, 1, 1]))
+    np.testing.assert_array_equal(
+        possible.to_numpy(), np.array([0, 1, 1, 0, 1, 2, 3, 0, 3, 3, 3, 3, 2, 1, 1])
+    )
 
-    def test_lag_count_category(self):
-        actual, possible = transitions.count_lags(
-            self.list_length, [self.pool_position], [self.output_position],
-            pool_test=[self.pool_category], recall_test=[self.output_category],
-            test=lambda x, y: x == y
-        )
 
-        np.testing.assert_array_equal(
-            actual.to_numpy(),
-            np.array([0, 0, 0, 0, 1, 0, 1, 0, 1, 1, 0, 0, 0, 0, 0]))
+def test_lag_count_category(data):
+    actual, possible = transitions.count_lags(
+        data['list_length'], [data['pool_position']], [data['output_position']],
+        pool_test=[data['pool_category']], recall_test=[data['output_category']],
+        test=lambda x, y: x == y
+    )
 
-        np.testing.assert_array_equal(
-            possible.to_numpy(),
-            np.array([0, 0, 0, 0, 1, 1, 3, 0, 2, 1, 1, 0, 0, 0, 0]))
+    np.testing.assert_array_equal(
+        actual.to_numpy(),
+        np.array([0, 0, 0, 0, 1, 0, 1, 0, 1, 1, 0, 0, 0, 0, 0])
+    )
+
+    np.testing.assert_array_equal(
+        possible.to_numpy(),
+        np.array([0, 0, 0, 0, 1, 1, 3, 0, 2, 1, 1, 0, 0, 0, 0])
+    )
