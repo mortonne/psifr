b5c1e64faa6408e9c7fc75562e2053f647876bda
Neal Morton <mortonne@gmail.com>
2021-04-16 22:45:20 -0500

    build: remove upload.sh script

--- upload.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-python3 -m pip install --user --upgrade setuptools wheel twine
-python3 setup.py sdist bdist_wheel
-python3 -m twine upload --repository-url https://upload.pypi.org/legacy/ dist/*
