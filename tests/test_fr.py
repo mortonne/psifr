db105a98c6cf1a3548f1edf153e09ef3ff180813
Neal Morton <mortonne@gmail.com>
2021-04-16 23:26:17 -0500

    test: move transitions tests to test_fr.py

--- tests/test_fr.py
+++ tests/test_fr.py
@@ -4,6 +4,7 @@ import numpy as np
 import pandas as pd
 import pytest
 from psifr import fr
+from psifr import transitions
 
 
 @pytest.fixture()
@@ -60,6 +61,17 @@ def distances():
                     [2, 2, 2, 2, 0, 2],
                     [2, 2, 2, 3, 2, 0]])
     return mat
+
+
+@pytest.fixture()
+def distances2():
+    distances = np.array([[0, 1, 3, 3, 3, 3],
+                          [1, 0, 3, 3, 3, 3],
+                          [3, 3, 0, 2, 1, 2],
+                          [3, 3, 2, 0, 2, 2],
+                          [3, 3, 1, 2, 0, 2],
+                          [3, 3, 2, 2, 2, 0]])
+    return distances
 
 
 def test_merge(raw):
@@ -123,11 +135,100 @@ def test_split_lists(data):
     np.testing.assert_allclose(recall['recalls'][0], np.array([2., 3., np.nan]))
 
 
+def test_lists_input(data):
+    m = transitions.TransitionMeasure('input', 'input')
+    pool_lists = m.split_lists(data, 'study')
+    pool_expected = {
+        'items': [[1.0, 2.0, 3.0], [1.0, 2.0, 3.0]],
+        'label': [[1.0, 2.0, 3.0], [1.0, 2.0, 3.0]],
+        'test': None
+    }
+    assert pool_lists == pool_expected
+
+    recall_lists = m.split_lists(data, 'recall')
+    recall_expected = {
+        'items': [[2.0, 3.0, np.nan], [3.0, 1.0, 3.0]],
+        'label': [[2.0, 3.0, np.nan], [3.0, 1.0, 3.0]],
+        'test': None
+    }
+    for key in recall_expected.keys():
+        assert key in recall_lists
+        np.testing.assert_array_equal(recall_lists[key], recall_expected[key])
+
+
 def test_pnr(data):
     stat = fr.pnr(data)
     expected = np.array([0, 0.5, 0.5, 0.5, 0, 1, np.nan, np.nan, np.nan])
     observed = stat['prob'].to_numpy()
     np.testing.assert_array_equal(expected, observed)
+
+
+def test_lag_crp(data):
+    crp = fr.lag_crp(data)
+    actual = np.array([1, 0, 0, 1, 0])
+    possible = np.array([1, 2, 0, 1, 0])
+    prob = np.array([1., 0., np.nan, 1., np.nan])
+
+    np.testing.assert_array_equal(crp['actual'], actual)
+    np.testing.assert_array_equal(crp['possible'], possible)
+    np.testing.assert_array_equal(crp['prob'], prob)
+
+
+def test_lag_crp_query(data):
+    crp = fr.lag_crp(data, item_query='input != 2')
+    actual = np.array([1, 0, 0, 0, 0])
+    possible = np.array([1, 0, 0, 0, 0])
+    prob = np.array([1., np.nan, np.nan, np.nan, np.nan])
+
+    np.testing.assert_array_equal(crp['actual'], actual)
+    np.testing.assert_array_equal(crp['possible'], possible)
+    np.testing.assert_array_equal(crp['prob'], prob)
+
+
+def test_lag_crp_cat(data):
+    crp = fr.lag_crp(data, test_key='task', test=lambda x, y: x == y)
+    actual = np.array([1, 0, 0, 0, 0])
+    possible = np.array([1, 0, 0, 0, 0])
+    prob = np.array([1., np.nan, np.nan, np.nan, np.nan])
+
+    np.testing.assert_array_equal(crp['actual'], actual)
+    np.testing.assert_array_equal(crp['possible'], possible)
+    np.testing.assert_array_equal(crp['prob'], prob)
+
+
+def test_distance_crp(data, distances2):
+    edges = [.5, 1.5, 2.5, 3.5]
+    crp = fr.distance_crp(data, 'item_index', distances2, edges, count_unique=False)
+
+    actual = np.array([0, 1, 1])
+    possible = np.array([1, 2, 1])
+    prob = np.array([0, .5, 1])
+
+    np.testing.assert_array_equal(crp['actual'], actual)
+    np.testing.assert_array_equal(crp['possible'], possible)
+    np.testing.assert_array_equal(crp['prob'], prob)
+
+
+def test_distance_crp_unique(data, distances2):
+    edges = [.5, 1.5, 2.5, 3.5]
+    crp = fr.distance_crp(data, 'item_index', distances2, edges, count_unique=True)
+
+    actual = np.array([0, 1, 1])
+    possible = np.array([1, 1, 1])
+    prob = np.array([0, 1, 1])
+
+    np.testing.assert_array_equal(crp['actual'], actual)
+    np.testing.assert_array_equal(crp['possible'], possible)
+    np.testing.assert_array_equal(crp['prob'], prob)
+
+
+def test_category_crp(data):
+    # TODO: replace with more diagnostic test
+    crp = fr.category_crp(data, 'task')
+
+    assert crp['prob'].iloc[0] == 1
+    assert crp['actual'].iloc[0] == 1
+    assert crp['possible'].iloc[0] == 1
 
 
 def test_lag_rank(data):
