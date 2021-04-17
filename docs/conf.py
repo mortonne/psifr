1560fc954051e17d17ab8375aa07182bf4cb6353
Neal Morton <mortonne@gmail.com>
2021-04-17 02:15:04 -0500

    build: bump minor version

--- docs/conf.py
+++ docs/conf.py
@@ -21,7 +21,7 @@ copyright = '2020, Neal Morton'
 author = 'Neal Morton'
 
 # The full version, including alpha/beta/rc tags
-release = 'v0.4.4'
+release = 'v0.5.0'
 
 
 # -- General configuration ---------------------------------------------------
