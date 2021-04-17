3e31dc320a92be805cd1bb4bd2fc86fafdc3d600
Neal Morton <mortonne@gmail.com>
2021-04-16 23:46:20 -0500

    test: convert individual analysis tests to pytest

--- tests/test_category_crp.py
+++ tests/test_category_crp.py
@@ -1,25 +1,25 @@
-import unittest
+import pytest
 from psifr import transitions
 
 
-class CategoryCRPTestCase(unittest.TestCase):
-
-    def setUp(self) -> None:
-        self.pool_position = [1, 2, 3, 4, 5, 6, 7, 8]
-        self.pool_category = [1, 1, 1, 1, 2, 2, 2, 2]
-        self.output_position = [1, 3, 4, 8, 5, 4, 2, 7, 6]
-        self.output_category = [1, 1, 1, 2, 2, 1, 1, 2, 2]
-
-    def test_category_count(self):
-        # six transitions, but only five have a valid within-category
-        # transition that is possible
-        actual, possible = transitions.count_category(
-            [self.pool_position], [self.output_position],
-            [self.pool_category], [self.output_category]
-        )
-        assert actual == 4
-        assert possible == 5
+@pytest.fixture()
+def data():
+    test_list = {
+        'list_length': 8,
+        'pool_position': [1, 2, 3, 4, 5, 6, 7, 8],
+        'pool_category': [1, 1, 1, 1, 2, 2, 2, 2],
+        'output_position': [1, 3, 4, 8, 5, 4, 2, 7, 6],
+        'output_category': [1, 1, 1, 2, 2, 1, 1, 2, 2],
+    }
+    return test_list
 
 
-if __name__ == '__main__':
-    unittest.main()
+def test_category_count(data):
+    # six transitions, but only five have a valid within-category
+    # transition that is possible
+    actual, possible = transitions.count_category(
+        [data['pool_position']], [data['output_position']],
+        [data['pool_category']], [data['output_category']]
+    )
+    assert actual == 4
+    assert possible == 5
--- tests/test_pair_crp.py
+++ tests/test_pair_crp.py
@@ -1,39 +1,37 @@
-import unittest
+import pytest
 import numpy as np
 from psifr import transitions
 
 
-class PairCRPTestCase(unittest.TestCase):
-    def setUp(self) -> None:
-        self.n_item = 8
-        self.pool_items = [list(range(0, 4)),
-                           list(range(4, 8))]
-        self.recall_items = [[0, 2, 3, 0],
-                             [7, 4, 5, 6]]
-
-    def test_pair_count(self):
-        actual, possible = transitions.count_pairs(self.n_item,
-                                                   self.pool_items,
-                                                   self.recall_items)
-        actual_expected = np.array([[0, 0, 1, 0, 0, 0, 0, 0],
-                                    [0, 0, 0, 0, 0, 0, 0, 0],
-                                    [0, 0, 0, 1, 0, 0, 0, 0],
-                                    [0, 0, 0, 0, 0, 0, 0, 0],
-                                    [0, 0, 0, 0, 0, 1, 0, 0],
-                                    [0, 0, 0, 0, 0, 0, 1, 0],
-                                    [0, 0, 0, 0, 0, 0, 0, 0],
-                                    [0, 0, 0, 0, 1, 0, 0, 0]])
-        possib_expected = np.array([[0, 1, 1, 1, 0, 0, 0, 0],
-                                    [0, 0, 0, 0, 0, 0, 0, 0],
-                                    [0, 1, 0, 1, 0, 0, 0, 0],
-                                    [0, 0, 0, 0, 0, 0, 0, 0],
-                                    [0, 0, 0, 0, 0, 1, 1, 0],
-                                    [0, 0, 0, 0, 0, 0, 1, 0],
-                                    [0, 0, 0, 0, 0, 0, 0, 0],
-                                    [0, 0, 0, 0, 1, 1, 1, 0]])
-        np.testing.assert_array_equal(actual, actual_expected)
-        np.testing.assert_array_equal(possible, possib_expected)
+@pytest.fixture()
+def data():
+    list_data = {
+        'n_item': 8,
+        'pool_items': [list(range(0, 4)), list(range(4, 8))],
+        'recall_items': [[0, 2, 3, 0], [7, 4, 5, 6]]
+    }
+    return list_data
 
 
-if __name__ == '__main__':
-    unittest.main()
+def test_pair_count(data):
+    actual, possible = transitions.count_pairs(
+        data['n_item'], data['pool_items'], data['recall_items']
+    )
+    actual_expected = np.array([[0, 0, 1, 0, 0, 0, 0, 0],
+                                [0, 0, 0, 0, 0, 0, 0, 0],
+                                [0, 0, 0, 1, 0, 0, 0, 0],
+                                [0, 0, 0, 0, 0, 0, 0, 0],
+                                [0, 0, 0, 0, 0, 1, 0, 0],
+                                [0, 0, 0, 0, 0, 0, 1, 0],
+                                [0, 0, 0, 0, 0, 0, 0, 0],
+                                [0, 0, 0, 0, 1, 0, 0, 0]])
+    possib_expected = np.array([[0, 1, 1, 1, 0, 0, 0, 0],
+                                [0, 0, 0, 0, 0, 0, 0, 0],
+                                [0, 1, 0, 1, 0, 0, 0, 0],
+                                [0, 0, 0, 0, 0, 0, 0, 0],
+                                [0, 0, 0, 0, 0, 1, 1, 0],
+                                [0, 0, 0, 0, 0, 0, 1, 0],
+                                [0, 0, 0, 0, 0, 0, 0, 0],
+                                [0, 0, 0, 0, 1, 1, 1, 0]])
+    np.testing.assert_array_equal(actual, actual_expected)
+    np.testing.assert_array_equal(possible, possib_expected)
