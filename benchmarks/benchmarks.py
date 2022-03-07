88c1c1cca301ee5dd68e1d006676952db8537f70
Neal Morton <mortonne@gmail.com>
2022-03-07 00:36:25 -0600

    style: black reformatting of benchmark code

--- benchmarks/benchmarks.py
+++ benchmarks/benchmarks.py
@@ -8,23 +8,30 @@ class TimeFR:
     An example benchmark that times the performance of various kinds
     of iterating over dictionaries in Python.
     """
+
     def setup(self):
         data_file = resource_filename('psifr', 'data/Morton2013.csv')
         self.raw = pd.read_csv(data_file, dtype={'category': 'category'})
         self.raw.category.cat.as_ordered(inplace=True)
         study = self.raw.query('trial_type == "study"').copy()
         recall = self.raw.query('trial_type == "recall"').copy()
-        data = fr.merge_lists(study, recall,
-                              list_keys=['list_type', 'list_category'],
-                              study_keys=['category'])
+        data = fr.merge_lists(
+            study,
+            recall,
+            list_keys=['list_type', 'list_category'],
+            study_keys=['category'],
+        )
         self.data = data
 
     def time_merge(self):
         study = self.raw.query('trial_type == "study"').copy()
         recall = self.raw.query('trial_type == "recall"').copy()
-        data = fr.merge_lists(study, recall,
-                              list_keys=['list_type', 'list_category'],
-                              study_keys=['category'])
+        data = fr.merge_lists(
+            study,
+            recall,
+            list_keys=['list_type', 'list_category'],
+            study_keys=['category'],
+        )
 
     def time_lag_crp(self):
         crp = fr.lag_crp(self.data)
