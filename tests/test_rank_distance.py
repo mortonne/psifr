20a7d9d681f029081e7d47c1fd409a6fce28f0b3
Neal Morton <mortonne@gmail.com>
2022-03-08 00:17:51 -0600

    test: rank based on distance to prior items

--- tests/test_rank_distance.py
+++ tests/test_rank_distance.py
@@ -106,3 +106,37 @@ def test_rank_distance_across(list_data, distances):
     )
     expected = np.array([1 - (0.5 / 3), 1 - (0.5 / 1)])
     np.testing.assert_allclose(ranks, expected)
+
+
+def test_rank_distance_shifted(list_data, distances):
+    """Test rank distance based on shifted recalls."""
+    list_data['recall_items'] = [[4, 3, 1, 7, 3, 0, 6, 2]]
+    distances[3, 1] = 4
+    distances[1, 3] = 4
+    # sequences: [[4, 3, 1], [3, 1, 7], [0, 6, 5]]
+    # -1
+    # actual: [4, 2, 2]
+    # possible: [
+    #     [1, 1, 2, 2, 2, 4],
+    #     [1, 1, 2, 2, 2],
+    #     [2, 3],
+    # ]
+    # rank: [0.0, 0.25, 1.0]
+    # -2
+    # actual: [2, 2, 1]
+    # possible: [
+    #     [2, 2, 2, 3, 3, 3],
+    #     [1, 1, 2, 2, 2],
+    #     [1, 2],
+    # ]
+    # rank: [0.8, 0.25, 1.0]
+    ranks = transitions.rank_distance_shifted(
+        distances,
+        2,
+        list_data['pool_items'],
+        list_data['recall_items'],
+        list_data['pool_items'],
+        list_data['recall_items'],
+    )
+    expected = np.array([[0.8, 0.0], [0.25, 0.25], [1.0, 1.0]])
+    np.testing.assert_allclose(ranks, expected)
