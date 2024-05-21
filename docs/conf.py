5ffb7be2a2e8d7fa08a7188d1e9fb9b5eed72eb2
Neal Morton <mortonne@gmail.com>
2024-05-21 15:40:32 -0500

    build: bump patch version

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2020, Neal Morton'
 author = 'Neal Morton'
 
 # The full version, including alpha/beta/rc tags
-release = 'v0.9.1'
+release = 'v0.9.2'
 
 
 # -- General configuration ---------------------------------------------------
