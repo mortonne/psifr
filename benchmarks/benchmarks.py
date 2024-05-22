9d0ebe1da81ed375b81e77a37febd76e47370c65
Neal Morton <mortonne@gmail.com>
2024-05-22 11:59:33 -0500

    perf: benchmark support for different studies and engines

--- benchmarks/benchmarks.py
+++ benchmarks/benchmarks.py
@@ -1,26 +1,46 @@
-from pkg_resources import resource_filename
+from importlib import resources
 import pandas as pd
 from psifr import fr
 
 
 class TimeFR:
-    """
-    An example benchmark that times the performance of various kinds
-    of iterating over dictionaries in Python.
-    """
+    """Time common manipulations of free-recall data."""
+
+    def __init__(self, study, engine='python', dtype_backend='numpy_nullable'):
+        self.study = study
+        self.data_file = str(resources.files('psifr') / 'data' / f'{study}.csv')
+        self.raw = None
+        self.data = None
+        self.category = True if study == 'Morton2013' else False
+        self.engine = engine
+        self.dtype_backend = dtype_backend
 
     def setup(self):
-        data_file = resource_filename('psifr', 'data/Morton2013.csv')
-        self.raw = pd.read_csv(data_file, dtype={'category': 'category'})
-        self.raw.category.cat.as_ordered(inplace=True)
-        self.data = fr.merge_free_recall(
-            self.raw, list_keys=['list_type', 'list_category'], study_keys=['category']
-        )
+        opt = {'engine': self.engine, 'dtype_backend': self.dtype_backend}
+        if self.category:
+            self.raw = pd.read_csv(
+                self.data_file, dtype={'category': 'category'}, **opt
+            )
+            self.raw.category = self.raw.category.cat.as_ordered()
+            self.data = fr.merge_free_recall(
+                self.raw,
+                list_keys=['list_type', 'list_category'],
+                study_keys=['category'],
+            )
+        else:
+            self.raw = pd.read_csv(self.data_file, **opt)
+            self.data = fr.merge_free_recall(self.raw)
+
+    def time_read(self):
+        raw = pd.read_csv(self.data_file)
 
     def time_merge(self):
-        data = fr.merge_free_recall(
-            self.raw, list_keys=['list_type', 'list_category'], study_keys=['category']
-        )
+        if self.category:
+            data = fr.merge_free_recall(
+                self.raw, list_keys=['list_type', 'list_category'], study_keys=['category']
+            )
+        else:
+            data = fr.merge_free_recall(self.raw)
 
     def time_lag_crp(self):
         crp = fr.lag_crp(self.data)
@@ -29,6 +49,8 @@ class TimeFR:
         crp = fr.lag_crp_compound(self.data)
 
     def time_category_crp(self):
+        if not self.category:
+            raise ValueError(f'Cannot run category crp test on {self.study} dataset.')
         crp = fr.category_crp(self.data, 'category')
 
     def time_spc(self):
